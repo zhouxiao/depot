0da53528e9a83be4f0b29d891188e337090236b7
U-CORP\XZhou <XZhou@PEK-Xzhou-L2.corp.ad.wrs.com>
2013-01-08 16:30:05 +0800

    Catalog Display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def showtime
+		t = Time.now
+		t_str = t.asctime
+		t_a = t_str.split(' ')
+		t_a.delete_at(3)
+		t_a[2] = "%02d" % t_a[2]
+		t_str = t_a.join(' ')
+		
+		return t_str 
+	end
+
 end
