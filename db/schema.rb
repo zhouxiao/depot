9d1dc17fc294bb922e8b349003dbeaa169382056
U-CORP\XZhou <XZhou@PEK-Xzhou-L2.corp.ad.wrs.com>
2013-01-14 17:57:13 +0800

    Cart Creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130108085218) do
+ActiveRecord::Schema.define(:version => 20130108090605) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
