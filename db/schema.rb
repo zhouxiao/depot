d96ef3a804351085c97cde97f4cab3cb31a5a82c
zhouxiao <randy.zhou@gmail.com>
2013-01-21 17:11:18 +0800

    Taks I: Logging In

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130117022351) do
+ActiveRecord::Schema.define(:version => 20130121072941) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -42,6 +43,14 @@ ActiveRecord::Schema.define(:version => 20130117022351) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
